8f046941176176284ecdcd82568efbb733a438c7
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2026-06-08 18:49:53 +0530

    feat: initialize routing and create administrator login page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./login-administrator/login-administrator').then(m => m.LoginAdministrator), }
+];
