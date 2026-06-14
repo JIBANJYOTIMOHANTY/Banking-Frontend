b4c41dc1d06c9a3cc5f8c1fd371658bd4aac00c9
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2026-06-14 13:49:14 +0530

    feat: add production API base URL to environment configuration

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // BASE_API_URL: 'http://localhost:8080/api/v1/',
-    BASE_API_URL: 'https://banking-backend-hrbj.onrender.com',
+    BASE_API_URL: 'https://banking-backend-hrbj.onrender.com/api/v1/',
 
 }
